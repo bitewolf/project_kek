72d62d0932445b25f5c69459371b20e2f5228677
bitewolf <2189108@gmail.com>
2020-07-05 00:22:52 +0300

    fix js

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,19 +2,17 @@ $(function() {
     var header = $("header"),
         introH = $("#intro").innerHeight(),
         scrollOffset = $(window).scrollTop();
+
+/*fixed header*/
         checkScroll(scrollOffset);
 
     $(window).on("scroll", function() {
-
         scrollOffset = $(this).scrollTop();
 
-/*fixed header*/
         checkScroll(scrollOffset);
     });
 
     function checkScroll(scrollOffset) {
-
-
         if (scrollOffset >= introH){
           header.addClass("fixed");
         } else {header.removeClass("fixed");
@@ -23,21 +21,21 @@ $(function() {
         }
     }
 
-/*smooth scroll*/
-$("[data-scroll]").on("click", function(event) {
-  event.preventDefault();
+    /*smooth scroll*/
+    $("[data-scroll]").on("click", function(event) {
+      event.preventDefault();
 
-  var $this = $(this),
-  blockId = $(this).data('scroll'),
-  blockOffset = $(blockId).offset().top;
+      var $this = $(this),
+      blockId = $(this).data('scroll'),
+      blockOffset = $(blockId).offset().top;
 
-  $("#nav a").removeClass("active");
-  $this.addClass("active");
+      $("#nav a").removeClass("active");
+      $this.addClass("active");
 
-    $("html, body").animate({
-      scrollTop: blockOffset
-    }, 500);
-});
+        $("html, body").animate({
+          scrollTop: blockOffset
+        }, 500);
+    });
 
 
 /* Menu nav toggle */
@@ -48,12 +46,13 @@ $("#nav_toggle").on("click", function(event) {
   $("#nav").toggleClass("active");
 });
 
+
 /* collapse */
 $("[data-collapse]").on("click", function(event) {
   event.preventDefault();
 
   var $this = $(this),
-  blockId = $(this).data('collapse');
+  blockId = $this.data('collapse');
 
   $(this).toggleClass("active");
 
